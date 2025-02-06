49f0cf4ba8dccb89a7b90e52b93ad738ed8b62ba
sergioxd09 <103318655+sergioxd09@users.noreply.github.com>
2025-02-06 12:06:22 -0500

    asd
    holaa

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test',function() {
+    return view('test');
+});
+
+Route:: get ('/crudy',function(){
+    $age=33;
+    $data=['name' => 'Sergio','age'=>20];
+    return view('crud/index',$data);
+})->name('crud');
