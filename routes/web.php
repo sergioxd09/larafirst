8d9da4570194cd9b5af8557e7847e8b932ed03b6
sergioxd09 <103318655+sergioxd09@users.noreply.github.com>
2025-02-19 10:47:17 -0500

    mi cerebro

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\PostController;
 use App\Http\Controllers\PrimerControlador;
 use App\Http\Controllers\SegundoControlador;
 use Illuminate\Support\Facades\Route;
@@ -31,3 +32,7 @@ Route::get('otro/{post}/{otro} ', [PrimerControlador::class,'otro'/*iNDEX nombre
 
 
 Route::resource('post', PrimerControlador::class);
+
+
+//jalando controlador para crud
+route::resource('post',PostController::class);
